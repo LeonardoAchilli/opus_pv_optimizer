3795a763c8118016050e9d00816aa37f61a68a58
LeonardoAchilli <128692783+LeonardoAchilli@users.noreply.github.com>
2025-06-18 15:47:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import json
 import numpy as np
 import pandas as pd
@@ -5,8 +6,6 @@ import streamlit as st
 import io
 import zipfile
 from typing import Dict, Tuple, Optional, List
-
-from __future__ import annotations
 
 import numpy as np
 import pandas as pd
